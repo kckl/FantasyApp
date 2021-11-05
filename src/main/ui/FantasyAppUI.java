d1ad2b04e89c37a2784966c45a3aedecf5a72184
kckl <85543667+kckl@users.noreply.github.com>
2021-11-05 14:17:08 -0700

    Phase3.created window

--- src/main/ui/FantasyAppUI.java
+++ src/main/ui/FantasyAppUI.java
@@ -3,21 +3,25 @@ package ui;
 import javax.swing.*;
 import java.awt.*;
 
-public class FantasyAppUI extends JPanel {
+public class FantasyAppUI extends JFrame {
     private FantasyApp fa;
-    private static final int WIDTH = 100;
-    private static final int HEIGHT = 30;
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 700;
     private JDesktopPane desktop;
+    private JFrame frame;
 
     public FantasyAppUI() {
-        fa = new FantasyApp();
-        desktop = new JDesktopPane();
-        setLayout(new BorderLayout());
+        super("NBA Fantasy Helper");
+        initializeGraphics();
+    }
 
-        setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        setBackground(Color.GRAY);
+    // MODIFIES: this
+    // EFFECTS:  draws the JFrame window where this Fantasy App will operate
+    private void initializeGraphics() {
+        setSize(WIDTH, HEIGHT);
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
     }
-
 }
