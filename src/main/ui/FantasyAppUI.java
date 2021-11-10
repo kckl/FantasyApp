ff0721583c72bc80c239cac9c80720117227cf4b
kckl <85543667+kckl@users.noreply.github.com>
2021-11-10 02:27:20 -0800

    Phase3.added menu bar

--- src/main/ui/FantasyAppUI.java
+++ src/main/ui/FantasyAppUI.java
@@ -1,27 +1,58 @@
 package ui;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FantasyAppUI extends JFrame {
-    private FantasyApp fa;
-    private static final int WIDTH = 1000;
-    private static final int HEIGHT = 700;
+// Represents the main frame for the fantasy application
+public class FantasyAppUI extends JFrame implements ActionListener {
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 500;
     private JDesktopPane desktop;
     private JFrame frame;
+    private JMenuBar menuBar;
 
+    // EFFECTS: sets up window for fantasy app
     public FantasyAppUI() {
-        super("NBA Fantasy Helper");
-        initializeGraphics();
+
+        frame = new JFrame();
+        desktop = new JDesktopPane();
+
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(desktop);                     // adds the desktop to the frame
+        frame.setTitle("NBA Fantasy Helper");
+
+        createMenuBar();
+
+        frame.setVisible(true);
     }
 
-    // MODIFIES: this
-    // EFFECTS:  draws the JFrame window where this Fantasy App will operate
-    private void initializeGraphics() {
-        setSize(WIDTH, HEIGHT);
-        setLayout(new BorderLayout());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+    // EFFECTS: creates a menu bar with menu items
+    private void createMenuBar() {
+        menuBar = new JMenuBar();
+
+        JMenu saveLoadMenu = new JMenu("Save/Load");
+        menuBar.add(saveLoadMenu);
+
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveLoadMenu.add(saveMenuItem);
+
+        JMenuItem loadMenuItem = new JMenuItem("Load");
+        saveLoadMenu.add(loadMenuItem);
+
+        JMenu settingsMenu = new JMenu("Settings");
+        menuBar.add(settingsMenu);
+
+        JMenuItem editMenuItem = new JMenuItem("Edit");
+        settingsMenu.add(editMenuItem);
+
+        frame.setJMenuBar(menuBar);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
