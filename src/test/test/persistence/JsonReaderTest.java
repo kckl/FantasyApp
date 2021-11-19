3651df90135457b78cbb473ac73c97c2273e9419
kckl <85543667+kckl@users.noreply.github.com>
2021-11-18 21:53:31 -0800

    Phase3.fixed test v3

--- src/test/test/persistence/JsonReaderTest.java
+++ src/test/test/persistence/JsonReaderTest.java
@@ -53,17 +53,17 @@ class JsonReaderTest extends JsonTest {
         }
     }
 
-//    @Test
-//    void testReaderTeamWithPlayer() {
-//        JsonReader reader = new JsonReader("./data/testReaderNormalLeague.json");
-//        try {
-//            League league = reader.read();
-//            Team team = league.getTeam(0);
-//            Player player1 = new Player("Curry", team);
-//            team.addPlayer(player1);
-//            assertEquals(1, team.getPlayerNames().size());
-//        } catch (IOException e) {
-//            fail("Couldn't read from file");
-//        }
-//    }
+    @Test
+    void testReaderTeamWithPlayer() {
+        JsonReader reader = new JsonReader("./data/testReaderNormalLeague.json");
+        try {
+            League league = reader.read();
+            Team team = league.getTeam(0);
+            Player player1 = new Player("Curry", team);
+            team.addPlayer(player1);
+            assertEquals(1, team.getPlayerNames().size());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
 }
